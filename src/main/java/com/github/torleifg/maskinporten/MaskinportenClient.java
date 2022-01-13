0c937b8207241e94c609bfcd79e07d37977ba111
Torleif Gravelsæter <torgravels@gmail.com>
2022-01-13 15:41:05 +0100

    Cache

--- src/main/java/com/github/torleifg/maskinporten/MaskinportenClient.java
+++ src/main/java/com/github/torleifg/maskinporten/MaskinportenClient.java
@@ -4,16 +4,17 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
 import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.oauth2.sdk.GeneralException;
 import com.nimbusds.oauth2.sdk.as.AuthorizationServerMetadata;
 import com.nimbusds.oauth2.sdk.id.Issuer;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.time.Clock;
-import java.util.Date;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class MaskinportenClient {
     protected String wellKnown;
@@ -24,17 +25,63 @@ public abstract class MaskinportenClient {
     protected JWSHeader header;
     protected RSASSASigner signer;
 
-    protected static final MaskinportenGateway GATEWAY = new MaskinportenGateway();
+    private final MaskinportenGateway gateway = new MaskinportenGateway();
+    private final Map<String, String> accessTokens = new ConcurrentHashMap<>();
 
     public Optional<String> getAccessToken(String... scopes) throws MaskinportenClientException {
+        if (cache) {
+            return getCacheAccessToken(scopes);
+        }
+
+        return gateway.getJwtGrantResponse(createJwtGrant(scopes), metadata.getTokenEndpointURI())
+                .map(JwtGrantResponse::getAccessToken);
+    }
+
+    private Optional<String> getCacheAccessToken(String... scopes) {
+        var key = String.join(" ", scopes);
+
+        var accessToken = accessTokens.get(String.join(" ", scopes));
+
+        if (accessToken == null) {
+            var jwtGrant = createJwtGrant(scopes);
+            var token = gateway.getJwtGrantResponse(jwtGrant, metadata.getTokenEndpointURI())
+                    .map(JwtGrantResponse::getAccessToken);
+
+            token.ifPresent(value -> accessTokens.put(key, value));
+
+            return token;
+        }
+
+        try {
+            var jwt = JWTParser.parse(accessToken);
+            var expiration = jwt.getJWTClaimsSet().getExpirationTime();
+
+            var current = new Date(Clock.systemUTC().millis() - 10000);
+
+            if (current.compareTo(expiration) >= 0) {
+                var jwtGrant = createJwtGrant(scopes);
+                var token = gateway.getJwtGrantResponse(jwtGrant, metadata.getTokenEndpointURI())
+                        .map(JwtGrantResponse::getAccessToken);
+
+                token.ifPresent(value -> accessTokens.put(key, value));
+
+                return token;
+            }
+
+        } catch (ParseException e) {
+            throw new MaskinportenClientException(e.getMessage(), e);
+        }
+
+        return Optional.of(accessToken);
+    }
+
+    private String createJwtGrant(String... scopes) {
         var claimsSet = createJWTClaimsSet(metadata.getIssuer().getValue(), clientId, scopes);
         var jwt = new SignedJWT(header, claimsSet);
 
         try {
             jwt.sign(signer);
-
-            return GATEWAY.getJwtGrantResponse(jwt.serialize(), metadata.getTokenEndpointURI())
-                    .map(JwtGrantResponse::getAccessToken);
+            return jwt.serialize();
 
         } catch (JOSEException e) {
             throw new MaskinportenClientException(e.getMessage(), e);
